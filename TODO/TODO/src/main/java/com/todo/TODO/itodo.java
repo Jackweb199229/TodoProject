3ac0cbd977306f0d5d2d3100190997856293e09c
MD Jafar <69416964+Jackweb199229@users.noreply.github.com>
2023-10-19 11:05:27 +0530

    Delete TODO directory

--- TODO/TODO/src/main/java/com/todo/TODO/itodo.java
+++
@@ -1,9 +0,0 @@
-package com.todo.TODO;
-
-//import java.util.List;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface itodo extends CrudRepository<todo, Integer> {
-//	List<todo> findByusername(String username);
-}	
